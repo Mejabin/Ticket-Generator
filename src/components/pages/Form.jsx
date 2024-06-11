efb25052983592d480e760701ce1a6ecbf492b71
sabbirsami <smd71430@gmail.com>
2024-06-11 10:31:14 +0600

    fix design

--- src/components/pages/Form.jsx
+++ src/components/pages/Form.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Form = () => {
-  const [submissionMessage, setSubmissionMessage] = useState('');
-  const [token, setToken] = useState('');
+  const [submissionMessage, setSubmissionMessage] = useState("");
+  const [token, setToken] = useState("");
 
   const handleSupportPost = async (e) => {
     e.preventDefault();
@@ -12,9 +12,9 @@ const Form = () => {
     const currentDate = new Date().toISOString();
     const newToken = generateToken();
 
-console.log(token);
+    console.log(token);
 
-//post method
+    //post method
     const data = {
       mobileNo,
       category,
@@ -36,92 +36,112 @@ console.log(token);
 
       if (result.success) {
         // Set the alert message with token at the top of the form
-        window.alert(`Thank you! Your data was submitted successfully. Your token number is: ${newToken}`);
+        window.alert(
+          `Thank you! Your data was submitted successfully. Your token number is: ${newToken}`
+        );
         // Set the token in the state
         setToken(newToken);
         // Reset the form
         form.reset();
       } else {
-        setSubmissionMessage("There was an error submitting the form. Please try again.");
-        setToken('');
+        setSubmissionMessage(
+          "There was an error submitting the form. Please try again."
+        );
+        setToken("");
       }
     } catch (error) {
       console.error("Error:", error);
-      setSubmissionMessage("There was an error submitting the form. Please try again.");
-      setToken('');
+      setSubmissionMessage(
+        "There was an error submitting the form. Please try again."
+      );
+      setToken("");
     }
   };
 
   // Function to generate a random token
   const generateToken = () => {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let newToken = "";
     for (let i = 0; i < 5; i++) {
-      newToken += characters.charAt(Math.floor(Math.random() * characters.length));
+      newToken += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
     }
     return newToken;
   };
 
   return (
-    <section className="container mx-auto p-12 mt-20">
-      <div className="max-w-lg mx-auto bg-gray-200 rounded-lg shadow-lg p-12">
-        <h1 className="text-2xl font-bold mb-6 text-center text-black">Support Panel</h1>
-        <form onSubmit={handleSupportPost} className="space-y-6">
-          <div>
-            <label
-              htmlFor="mobile_no"
-              className="block text-black text-xl font-semibold mb-2"
-            >
-              Your Phone Number
-            </label>
-            <input
-              type="text"
-              placeholder="Please Enter Your Phone Number"
-              name="mobile_no"
-              className="w-full px-4 py-2 border rounded text-black"
-              required
-            />
-          </div>
-          <div className="">
-            <label className="block text-black text-xl font-semibold mb-2">
-              Select a category
-            </label>
-            <select
-              className="w-full px-4 py-2 border border-gray-700 rounded text-black"
-              name="category"
-              required
-            >
-              <option value="" disabled selected>
-                Select one
-              </option>
-              <option value="Hardware">Hardware</option>
-              <option value="Software">Software</option>
-            </select>
-          </div>
-          <div className="mt-8">
-            <button
-              className="w-full bg-black text-white px-4 py-2 rounded hover:bg-black"
-              type="submit"
-            >
-              SUBMIT
-            </button>
-          </div>
-        </form>
-        {/* Display token and reset button if token exists */}
-        {token && (
-          <div className="mt-4 text-center">
-            <p>Token Number: {token}</p>
-            <button
-              className="bg-red-900 text-white px-4 py-2 rounded hover:bg-red-900 mt-2"
-              onClick={() => {
-                setToken('');
-                setSubmissionMessage('');
-              }}
-            >
-              OK
-            </button>
-          </div>
-        )}
+    <section className="container mx-auto p-12 mt-32 h-[90vh]  overflow-hidden ">
+      <div className="max-w-md mx-auto bg-white rounded-lg shadow-2xl shadow-emerald-800/45  pt-0">
+        <div className=" bg-emerald-600 rounded-t-lg">
+          <h1 className="text-xl mb-6 py-4 px-10 text-white">
+            Send us a message
+          </h1>
+        </div>
+        <div className="px-8 py-10 pt-4">
+          <form onSubmit={handleSupportPost} className="space-y-6">
+            <div>
+              <label
+                htmlFor="mobile_no"
+                className="block text-black/50 text-xs uppercase font-bold mb-2"
+              >
+                Your Phone Number
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your phone number"
+                name="mobile_no"
+                className="w-full px-4 py-3 border rounded text-black placeholder:text-sm"
+                required
+              />
+            </div>
+            <div className="">
+              <label className="block text-black/50 text-xs uppercase font-bold mb-2">
+                Select a category
+              </label>
+              <select
+                className="w-full px-4 py-3 border border-gray-200 rounded text-black"
+                name="category"
+                required
+              >
+                <option
+                  className="opacity-25 disabled:opacity-35"
+                  value=""
+                  disabled
+                  selected
+                >
+                  Select one
+                </option>
+                <option value="Hardware">Hardware</option>
+                <option value="Software">Software</option>
+              </select>
+            </div>
+            <div className="mt-8">
+              <button
+                className="w-full bg-emerald-600 text-white px-4 py-3 rounded hover:bg-emerald-700 shadow-md shadow-emerald-800/25"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          {/* Display token and reset button if token exists */}
+          {token && (
+            <div className="mt-4 text-center">
+              <p>Token Number: {token}</p>
+              <button
+                className="bg-red-900 text-white px-4 py-2 rounded hover:bg-red-900 mt-2"
+                onClick={() => {
+                  setToken("");
+                  setSubmissionMessage("");
+                }}
+              >
+                OK
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
